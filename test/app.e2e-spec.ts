46ed9930ddfacc90d96cf7ec4632f3eb0902fefb
Dejan Toteff <deyan8284@gmail.com>
2020-03-21 21:46:39 +0200

    chore: import eslint config

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,21 +7,27 @@ const URL = 'http://localhost:8080'
 const WORD_PROFILE = `${URL}/word-profile`
 
 describe.skip('Word profile', () => {
-  it('/all (GET)', async () => {
+  it('/all (GET)', async() => {
     const {data} = await axios.get(`${WORD_PROFILE}/all`)
     expect(data.length).toBeGreaterThan(100)
   })
 
-  it('/add/:word (POST)', async () => {
-    const a = await axios.post(`${WORD_PROFILE}/add`, {a: 1, token: process.env.API_ACCESS_TOKEN})
+  it('/add/:word (POST)', async() => {
+    const a = await axios.post(`${WORD_PROFILE}/add`, {
+      a: 1,
+      token: process.env.API_ACCESS_TOKEN,
+    })
   })
 
-  test.skip('create/delete word profile instance', async () => {
+  test.skip('create/delete word profile instance', async() => {
     const toSave = {
       word: 'baz',
       related: [{translated: 'foo'}],
     }
-    const {data: saved} = await axios.post(`${URL}/word-profile/create`, toSave)
+    const {data: saved} = await axios.post(
+      `${URL}/word-profile/create`,
+      toSave
+    )
     console.log(saved)
     expect(pass(saved)({related: Array})).toBeTruthy()
     expect(pass(saved)({related: [{translated: String}]})).toBeTruthy()
