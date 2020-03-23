ed51a6b967d7e4d695eb50c14edaab41b76468ad
Dejan Toteff <deyan8284@gmail.com>
2020-03-23 21:58:55 +0200

    feat@stop  | speed reader

--- libs/speed-reader/src/speed-reader.service.spec.ts
+++ libs/speed-reader/src/speed-reader.service.spec.ts
@@ -1,20 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SpeedReaderService } from './speed-reader.service';
-import { FsService } from 'lib/fs';
+import {Test, TestingModule} from '@nestjs/testing'
+import {SpeedReaderService} from './speed-reader.service'
+import {FsService} from 'lib/fs'
 
 describe('SpeedReaderService', () => {
-  let service: SpeedReaderService;
+  let service: SpeedReaderService
 
-  beforeEach(async () => {
+  beforeEach(async() => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SpeedReaderService, FsService],
-    }).compile();
+    }).compile()
 
-    service = module.get<SpeedReaderService>(SpeedReaderService);
-  });
+    service = module.get<SpeedReaderService>(SpeedReaderService)
+  })
 
-  it('read book', async () => {
+  test('read book', async() => {
     const result = await service.readBook(1)
-    expect(result.length).toBeGreaterThan(1999)
-  });
-});
+    expect(result[11]).toBe('безграничните')
+    expect(result[12]).toBe('способности')
+    expect(result.length).toBe(67400)
+  })
+
+  test('read book - fail', async() => {
+    const result = await service.readBook('foo')
+    expect(result).toBe('')
+  })
+})
--- libs/speed-reader/src/speed-reader.service.ts
+++ libs/speed-reader/src/speed-reader.service.ts
@@ -1,34 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { FsService } from 'lib/fs';
+import {flatten, trim} from 'rambdax'
+import {Injectable} from '@nestjs/common'
+import {FsService} from 'lib/fs'
 
 const hash = {
-  0    : 'udhaus_fred',
-  1    : 'udhaus_napred',
-  2    : 'udhaus_pari',
-  3    : 'udhaus_pylnolunie',
-  4    : 'udhaus_radost',
-  5    : 'udi',
-  6    : 'voltaire',
-  7    : 'sunset',
-  8    : 'too_human',
-  9    : 'zaratustra',
-  demo : 'demo',
+  0: 'udhaus_fred',
+  1: 'udhaus_napred',
+  2: 'udhaus_pari',
+  3: 'udhaus_pylnolunie',
+  4: 'udhaus_radost',
+  5: 'udi',
+  6: 'voltaire',
+  7: 'sunset',
+  8: 'too_human',
+  9: 'zaratustra',
+  10: 'gogol',
+  demo: 'demo',
 }
- 
+
 @Injectable()
 export class SpeedReaderService {
   constructor(private fsService: FsService) {}
 
-  async readBook<K>(id: string|number){
-    if(!hash[id]) return ''
+  async readBook<K>(id: string | number) {
+    if (!hash[id]) return ''
 
     try {
-      return this.fsService.readFromData(
+      const contentRaw = await this.fsService.readFromData(
         `books/${hash[id]}.txt`
       )
+      const content = contentRaw.split` `.map(trim).map(x => x.split('\n'))
+      const result = flatten(content).map(trim)
+      return result
     } catch (e) {
-      console.log(e,'read.book')
-      return ''  
+      console.log(e, 'read.book')
+      return ''
     }
   }
 }
