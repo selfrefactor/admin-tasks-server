def27995b0d59a403b30fa547b982d29d1b64687
Dejan Toteff <deyan8284@gmail.com>
2020-03-23 23:54:41 +0200

    feat: basic log service

--- src/lambdas/lambdas.controller.ts
+++ src/lambdas/lambdas.controller.ts
@@ -2,6 +2,14 @@ import {defaultTo} from 'rambdax'
 import {Controller, Post, Body, Res} from '@nestjs/common'
 import {Response} from 'express'
 import {SpeedReaderService} from 'lib/speed-reader'
+import { log } from 'helpers'
+
+function logService(label, input){
+  log('sep')
+  log(`Incoming request '${label}'`, 'back')
+  console.log(input)  
+  log('sep')  
+}
 
 @Controller('lambdas')
 export class LambdasController {
@@ -9,6 +17,7 @@ export class LambdasController {
 
   @Post('speed-reader')
   async createInstance(@Body() input: {id: number}, @Res() res: Response) {
+    logService('speed.reader', input)
     if (!input) return res.status(400).send()
     const bookIndex = defaultTo(0, Number(input.id))
     const result = await this.speedReader.readBook(bookIndex)
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule)
+  app.enableCors();
   await app.listen(PORT, '0.0.0.0')
 }
 bootstrap()
