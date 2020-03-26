08e6069cf506d75e4e7a71ae19f36c9e41330dea
Dejan Toteff <deyan8284@gmail.com>
2020-03-27 00:02:43 +0200

    feat: read word profile dispite circular dependency during test

--- libs/fs-db/src/fs-db.service.ts
+++ libs/fs-db/src/fs-db.service.ts
@@ -4,6 +4,8 @@ import { DATA_LOCATION } from 'lib/constants';
 
 type AllowedLabels = 'word.profile' | 'translations' | 'i.learn.smarter'
 
+export const wordNotFound = word => `Such word '${word}' is not found`
+
 @Injectable()
 export class FsDbService {
   constructor(){
@@ -11,5 +13,15 @@ export class FsDbService {
   }
   async getKeys(label: AllowedLabels){
     return loadKeys(label)
+  }
+  wordProfile(){
+    return {
+      getAllWords: async () => this.getKeys('word.profile'),
+      getWord: async (word: string) => {
+        const maybeResult = await loadJson('word.profile', word)
+        if(!maybeResult) throw new Error(wordNotFound(word))
+        return maybeResult
+      }
+    }
   }
 }
