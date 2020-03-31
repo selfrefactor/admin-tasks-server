0b2e27e11d60fcb3156e41038a4e0a428a9503db
Dejan Toteff <deyan8284@gmail.com>
2020-03-31 23:23:57 +0300

    feat: niketa experimental theme

--- lambdas/niketa-theme/niketaTheme.ts
+++ lambdas/niketa-theme/niketaTheme.ts
@@ -5,34 +5,19 @@ import {readJsonSync, outputJsonSync} from 'fs-extra'
 
 const darkModeEnv = process.env.NIKETA_DARK === 'ON'
 
-const advanced = [
-  'advanced.bat',
-  'advanced.cat',
-  'advanced.dog',
-  'advanced.engine',
-  'advanced.hook',
-  'advanced.immigrant',
-  'advanced.mystery',
+const lightThemesList = [
+  'communication.breakdown',
+  'funky.drummer',
+  'glass.onion',
+  'dancing.days',
+  'hello.spaceboy'
 ]
-const brave = [
-  'brave.habits',
-  'brave.homer',
-  'brave.love',
-  'brave.neighbour',
-]
-const circus = [
-  'circus.ajax',
-  'circus.brother',
-  'circus.people',
-  'circus.whisky',
-]
-const niketa = ['niketa.owl', 'niketa.bear', 'niketa.moon']
 
 const american = ['american.alien', 'american.dad', 'american.spy']
 const hunger = ['hunger.force', 'hunger.shake', 'hunger.epiphany']
 const south = ['south.park', 'south.tegridy', 'south.authority']
 
-const allLightThemes = shuffle([...advanced, ...brave, ...circus, ...niketa])
+const allLightThemes = shuffle(lightThemesList)
 const allDarkThemes = shuffle([...american, ...south, ...hunger])
 
 const {HOME} = process.env
