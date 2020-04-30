e41c5570fcd8c8e168a382ea90561a2f47df3669
Dejan Toteff <deyan8284@gmail.com>
2020-04-30 13:58:10 +0300

    feat: check if ngrok is running

--- libs/ngrok/src/index.ts
+++ libs/ngrok/src/index.ts
@@ -1,9 +1,24 @@
 import * as ngrokLib from 'ngrok'
+import axios from 'axios'
 import {DEFAULT_PORT} from 'lib/constants'
+
+async function checkAvailable(){
+  try {
+    const {status} = await axios.get('https://toteff.eu.ngrok.io/')
+    return status === 200 
+  } catch (error) {
+    return false
+  }
+}
 
 export const ngrok = async(port = DEFAULT_PORT) => {
   const token = process.env.NGROK_TOKEN
   if (!token) return console.log('!token', token)
+  const alreadyUp = await checkAvailable()
+
+  if(alreadyUp){
+    return console.log('Already running in other instance or computer')
+  } 
 
   const url = await ngrokLib.connect({
     // proto: 'http',
