5b0660664ccff3803b62a2aa5d900427234dc2b7
Dejan Totev <dejan.totev@psinteractive.io>
2020-12-14 16:08:29 +0200

    feat: add sample wildcard route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,19 @@ import {AppService} from './app.service'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('ci')
-  getContiniousIntegrationStatus(): object {
-    return this.appService.getContiniousIntegrationStatus()
+  @Get('callback')
+  getCallback(): string {
+    return 'You hit callback route'
+  }
+
+  @Get('sign-out')
+  getSignOut(): string {
+    return 'You hit sign-out route'
+  }
+
+  @Get('*')
+  getAny(): string {
+    return 'You hit wildcard route'
   }
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,9 @@
 import {Injectable} from '@nestjs/common'
 import {readJson} from 'fs-extra'
-import {resolve} from 'path'
-import {existsSync} from 'fs'
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello Foo'
-  }
-  async getContiniousIntegrationStatus(): Promise<object> {
-    const location = resolve(`${process.env.HOME}/work/ci/docker-data.json`)
-    if(!existsSync(location)) return {error:'No docker data file'}
-
-    return readJson(location)
   }
 }
