ec807887338b5a0a6753cff0db8e512bf5b34162
Dejan Totev <dejan.totev@psinteractive.io>
2021-10-13 13:19:01 +0300

    chore@dnd

--- lambdas/cron/cron.ts
+++ lambdas/cron/cron.ts
@@ -3,14 +3,14 @@ import {niketaTheme} from '../niketa-theme/niketaTheme'
 import {log} from 'helpers-fn'
 import { killCode, startCode } from './kill-code'
 
-const fallbackEveryMinutes = 30
+const fallbackEveryMinutes = 25
 
 const tickInput =
   process.env.NIKETA_THEME_CRON === undefined
     ? fallbackEveryMinutes
     : Number(process.env.NIKETA_THEME_CRON)
     
-console.log(tickInput, 'NIKETA_THEME_CRON')
+console.log(tickInput, 'NIKETA_THEME_CRON minutes')
 const tick = Math.floor(tickInput * 60000)
 
 export async function cron(devMode: boolean) {
@@ -24,6 +24,7 @@ export async function cron(devMode: boolean) {
       await killCode()
       await delay(5000)
       await startCode()
+      log(`VSCode restarted`, 'box')
     }
     log(`${tickInput * i} minutes since start`, 'back')
     await delay(tick)
