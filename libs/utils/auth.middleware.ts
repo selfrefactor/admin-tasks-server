ec2f7417837d915a0c7c5d8c4038ad96b841b957
Deyan Totev <deyan.totev.extern@seven.one>
2022-04-27 11:11:47 +0300

    chore@small

--- libs/utils/auth.middleware.ts
+++ libs/utils/auth.middleware.ts
@@ -12,7 +12,6 @@ export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     if (req.method !== 'POST') return next()
     const password = path<string>('body.password', req)
-    console.log(`password`, password)
     if (
       [
         process.env.API_ACCESS_TOKEN,
